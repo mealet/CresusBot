7590842e69b01c7a87c4cea4454b5190e0ffc0f6
jasonragonter <jasonragonter@gmail.com>
2023-01-15 16:16:11 +0300

    Update v1.3
     - Вписан токен стандартным видом в коде
     - Отключён модуль websocket_local, а также убрана функция keep_alive()

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from disnake import ButtonStyle
 from disnake import TextInputStyle
 import datetime
 import os
-from websocket_local import keep_alive
 
 intents = disnake.Intents.all()
 command_sync_flags = commands.CommandSyncFlags.none()
@@ -180,5 +179,4 @@ async def reg(inter):
       "Комманда создана для канала <#1059752620127965285>", ephemeral=True)
 
 
-keep_alive()
-bot.run(os.getenv("token"))
+bot.run("MTA2MTY3MzQ1NjY5OTE4MzIyNw.G43uc2.-V2tlAhBhvaLcW_4C1YmGbb2IHDnbRG1nH167A")
